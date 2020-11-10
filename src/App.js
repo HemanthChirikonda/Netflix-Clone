17cd5552d0088b35043cdc50cb931aa994d2018a
your_username <hemanthchirikonda@gmail.com>
2020-11-10 22:32:08 +0530

    project completed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import React from 'react'
-
-import './App.css';
-import Row from './components/Row'
-import requests from './requests'
-
+import React, { useState } from "react";
+import Banner from "./components/Banner";
+import Row from "./components/Row";
+import NavBar from "./components/NavBar";
+import requests from "./requests";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>{'Hello'}</h1>
-      <Row title={'NETFLIX ORIGINALS'} fetchUrl={requests.fetchNetflixOriginals} />
-    </div>
-  );
+	return (
+		<div className="app">
+			<NavBar />
+			<Banner />
+			<Row
+				title={"NETFLIX ORIGINALS"}
+				fetchUrl={requests.fetchNetflixOriginals}
+				isLargeRow
+			/>
+			<Row title={"Tranding Now"} fetchUrl={requests.fetchTrending} />
+			<Row title={"Top Rated"} fetchUrl={requests.fetchTopRated} />
+			<Row title={"Action Movies"} fetchUrl={requests.fetchActionMovies} />
+			<Row title={"Comedy Movies"} fetchUrl={requests.fetchComedyMovies} />
+			<Row title={"Horror Movies"} fetchUrl={requests.fetchHorrorMovies} />
+			<Row title={"Romance Movies"} fetchUrl={requests.fetchRomanceMovies} />
+			<Row title={"Documentaries"} fetchUrl={requests.fetchDocumentaries} />
+		</div>
+	);
 }
 
 export default App;
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,32 +1,94 @@
-import React, { useState, useEffect } from 'react'
-import axios from '../axios'
+import React, { useState, useEffect } from "react";
+import YouTube from "react-youtube";
 
+// import getYouTubeID from "get-youtube-id";
+import axios from "../axios";
+import "./Row.css";
+import movieTrailer from "movie-trailer";
 
+const base_url = "https://image.tmdb.org/t/p/original";
 
+function Row({ title, fetchUrl, isLargeRow }) {
+	const [movies, setMovies] = useState([]);
+	const [trailerURL, settrailerURL] = useState("");
+	//   a snippet of code which run based on specific conditions
+	useEffect(() => {
+		//if [],runs once when the row loads then dont run again
+		const fetchData = async () => {
+			const request = await axios.get(fetchUrl);
+			setMovies(request.data.results);
+			// console.log(request);
+		};
 
-function Row({ title, fetchUrl }) {
-    const [movies, setMovies] = useState([]);
+		fetchData();
+	}, [fetchUrl]);
 
+	const opts = {
+		height: "390",
+		width: "100%",
+		playerVars: {
+			// https://developers.google.com/youtube/player_parameters
+			autoplay: 1,
+		},
+	};
 
-    //   a snippet of code which run based on specific conditions
-    useEffect(() => {
-        //if [],runs once when the row loads then dont run again
-        const fetchData = async () => {
-            const reuest = await axios.get(fetchUrl);
-        };
+	const handlePlay = (movie) => {
+		console.log(movie);
+		if (trailerURL) {
+			settrailerURL("");
+		} else {
+			movieTrailer(`${movie.name ? movie.name : movie.original_title}`)
+				.then((url) => {
+					const urlParams = new URLSearchParams(new URL(url).search);
+					settrailerURL(urlParams.get("v"));
+				})
+				.catch((err) => {
+					alert("url not found");
+				});
+			///https://www.youtube.com/watch?v=X4bF_quwNtw&t=2s
+			// async function data() {
+			// 	let res = await movieTrailer(movie.name);
+			// }
 
-        fetchData();
+			// data();
+		}
+	};
 
-    }, [])
+	return (
+		<div className="row">
+			{/* title */}
+			<h2>{title}</h2>
+			{/* container --> posters */}
 
-
-    return (
-        <div>
-            {/* title */}
-            <h2>{title}</h2>
-            {/* containre --> posters */}
-        </div>
-    )
+			<div className="row_posters">
+				{movies.length > 0
+					? movies?.map((movie) => {
+							return (
+								<img
+									className={
+										isLargeRow ? "row_poster row_posterLarge" : "row_poster"
+									}
+									key={movie.id}
+									onClick={() => {
+										handlePlay(movie);
+									}}
+									src={
+										movie
+											? `${base_url}${
+													isLargeRow ? movie.poster_path : movie?.backdrop_path
+											  }`
+											: ""
+									}
+									alt={movie.name}
+									// onClick={() => handlePlay(movie.name)}
+								/>
+							);
+					  })
+					: ""}
+			</div>
+			{trailerURL ? <YouTube videoId={trailerURL} opts={opts} /> : ""}
+		</div>
+	);
 }
 
 export default Row;
